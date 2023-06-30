3c5734b71179e5d93bb56f0375504ede68d3aa39
ienrum <azang0506@gmail.com>
2023-06-30 15:24:24 +0900

    feat: tag 와 list 데이터를 react 어느곳에서나 사용 가능하도록 따로 파일 추가

---
+++ src/datas/List.js
@@ -0,0 +1,7 @@
+import { TAGS } from "./Tags";
+export const defaultTimeLine = [
+  { id: 0, duration: 120, tag: TAGS.STUDY, startedAt: Date() },
+  { id: 1, duration: 30, tag: TAGS.EXERCISE, startedAt: Date() },
+  { id: 2, duration: 60, tag: TAGS.FRIENDS, startedAt: Date() },
+  { id: 3, duration: 10, tag: TAGS.WORK, startedAt: Date() },
+];
---
+++ src/datas/Tags.js
@@ -0,0 +1,7 @@
+export const TAGS = {
+  ALL: "All",
+  STUDY: "Study",
+  WORK: "Work",
+  EXERCISE: "Exercise",
+  FRIENDS: "Friends",
+};
