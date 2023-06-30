6dc38707ca78f830b1b8263d2ac1f29ae3d5c481
ienrum <azang0506@gmail.com>
2023-06-30 15:29:28 +0900

    feat: 모든 컴포넌트 를 app 에 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import "./App.css";
 import StopWatch from "./components/StopWatch";
+import TimeLine from "./components/TimeLine";
+import TagOptions from "./components/TagOptions";
+import { useState } from "react";
+import { TAGS } from "./datas/Tags";
+import { defaultTimeLine } from "./datas/List";
+import Card from "./components/Card";
 
 function App() {
+  const [timeLine, setTimeLine] = useState(defaultTimeLine);
+  const [tagOption, setTagOption] = useState(TAGS.ALL);
   return (
     <div className="App">
       <h1>Stop Watch Timer</h1>
-      <StopWatch />
+      <StopWatch onSetTimeLine={setTimeLine} tag={tagOption} />
+      <Card className="notHover">
+        <TagOptions onSetTagOption={setTagOption} />
+        <TimeLine
+          onSetTimeLine={setTimeLine}
+          timeLine={timeLine}
+          tagOption={tagOption}
+        />
+      </Card>
     </div>
   );
 }
