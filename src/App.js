12b197b5c861b530b6460234c7914c4bb145330c
ienrum <azang0506@gmail.com>
2023-07-02 01:00:34 +0900

    feat: 로컬 스토리지에 데이터 저장및 가져오기

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import "./App.css";
 import StopWatch from "./components/StopWatch";
 import TimeLine from "./components/TimeLine";
 import TagOptions from "./components/TagOptions";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TAGS } from "./datas/Tags";
 import { defaultTimeLine } from "./datas/List";
 import Card from "./components/Card";
 
 function App() {
-  const [timeLine, setTimeLine] = useState(defaultTimeLine);
+  const [timeLine, setTimeLine] = useState(
+    JSON.parse(window.localStorage.getItem("timeLine"))
+  );
   const [tagOption, setTagOption] = useState(TAGS.ALL);
+
+  useEffect(() => {
+    window.localStorage.setItem("timeLine", JSON.stringify(timeLine));
+  }, [timeLine]);
+
   return (
     <div className="App">
       <h1>Stop Watch Timer</h1>
