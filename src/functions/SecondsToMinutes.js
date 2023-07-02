97aa1af90580db70780e1b370c539c23a7306aaa
ienrum <azang0506@gmail.com>
2023-07-02 10:25:52 +0900

    fix: hour 추가

--- src/functions/SecondsToMinutes.js
+++ src/functions/SecondsToMinutes.js
@@ -1,8 +1,10 @@
 const SecondsToMinutes = (seconds) => {
   const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const minutesLeft = minutes % 60;
   const secondsLeft = seconds % 60;
   const secondsFormatted = secondsLeft < 10 ? `0${secondsLeft}` : secondsLeft;
-  return `${minutes}:${secondsFormatted}`;
+  return `${hours > 0 ? hours + ":" : ""}${minutesLeft}:${secondsFormatted}`;
 };
 
 export default SecondsToMinutes;
