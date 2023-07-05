061e6646875d41e78234c84f79f4d0573604bcd6
ienrum <137467530+ienrum@users.noreply.github.com>
2023-07-05 21:36:16 +0900

    Test (#1)
    * fix: add minutesFormatted feature
    
    * debug: add debug setting fies
    
    * fix: change ico file
    
    * feat: add color datas
    
    * refactor: change useState to Reducer
    
    * refactor: refact intervals to intervalId and fix bugs
    
    언마운트 될때 콜백을 줘서 버튼이 이상하게 실행됨 , 따라서 마운트 될때로 고치고, 인터벌 state 를 리스트에서 단일 id int 로 바꿈

---
+++ src/datas/TagsColor.js
@@ -0,0 +1,9 @@
+import { TAGS } from "./Tags";
+
+export const COLORS = {
+  [TAGS.ALL]: "#e0e0e0",
+  [TAGS.STUDY]: "#ffe0e0",
+  [TAGS.EXERCISE]: "#e0f0ff",
+  [TAGS.WORK]: "#e0ffe0",
+  [TAGS.FRIENDS]: "#ffe0d0",
+};
--- src/functions/SecondsToMinutes.js
+++ src/functions/SecondsToMinutes.js
@@ -2,9 +2,12 @@ const SecondsToMinutes = (seconds) => {
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const minutesLeft = minutes % 60;
+  const minutesFormatted = minutesLeft < 10 ? `0${minutesLeft}` : minutesLeft;
   const secondsLeft = seconds % 60;
   const secondsFormatted = secondsLeft < 10 ? `0${secondsLeft}` : secondsLeft;
-  return `${hours > 0 ? hours + ":" : ""}${minutesLeft}:${secondsFormatted}`;
+  return `${
+    hours > 0 ? hours + ":" : ""
+  }${minutesFormatted}:${secondsFormatted}`;
 };
 
 export default SecondsToMinutes;
