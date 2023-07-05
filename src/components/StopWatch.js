7227f7522a89c71ecc9151c1f2ae9cabb7a08d75
ienrum <azang0506@gmail.com>
2023-07-05 18:42:40 +0900

    refactor: change useState to Reducer

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -1,59 +1,84 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
 import SecondsToMinutes from "../functions/SecondsToMinutes";
 import Card from "./UI/Card";
 import styles from "./StopWatch.module.css";
 const { v4: uuidv4 } = require("uuid");
 
+const buttonReducer = (prevState, action) => {
+  return {
+    isStop: prevState.isStop ? false : true,
+    startedAt: Date.now(),
+  };
+};
+
 const StopWatch = (props) => {
   const [currentTime, setCurrentTime] = useState(0);
-  const [isStop, setIsStop] = useState(true);
-  const [intervalEl, setIntervalEl] = useState(null);
-  const [startedAt, setStartedAt] = useState(Date());
+  const [intervals, setIntervals] = useState([]);
+  let cnt = 0;
+  const [buttonState, dispatchButton] = useReducer(buttonReducer, {
+    type: "STOP",
+    isStop: true,
+    startedAt: Date(),
+    a,
+  });
+  // useEffect(() => {
+  //   window.addEventListener(
+  //     "focus",
+  //     function () {
+  //       const duration = Math.floor(
+  //         (Date.now() - buttonState.startedAt) / 1000
+  //       );
+  //       setCurrentTime((prevState) => (prevState === 0 ? 0 : duration));
+  //     },
+  //     false
+  //   );
+  // }, [buttonState.isStop]);
 
   useEffect(() => {
-    window.addEventListener(
-      "focus",
-      function () {
-        const duration = Math.floor((Date.now() - startedAt) / 1000);
-        setCurrentTime((prevState) => (prevState === 0 ? 0 : duration));
-      },
-      false
-    );
-  }, [isStop]);
-
-  const stopWatchButtonHandler = () => {
-    if (isStop) {
-      setStartedAt(Date.now());
-      setIntervalEl(
-        setInterval(() => {
-          setCurrentTime((prevState) => prevState + 1);
-        }, 1000)
-      );
-    } else {
-      clearInterval(intervalEl);
-      if (currentTime > 0) {
-        props.onSetTimeLine((prevState) => [
-          ...prevState,
-          {
-            id: uuidv4(),
-            duration: currentTime,
-            tag: props.tag,
-            startedAt: startedAt,
-          },
-        ]);
+    return () => {
+      if (buttonState.isStop) {
+        if (currentTime > 0) {
+          props.onSetTimeLine((prevState) => [
+            ...prevState,
+            {
+              id: uuidv4(),
+              duration: currentTime,
+              tag: props.tag,
+              startedAt: buttonState.startedAt,
+            },
+          ]);
+        }
+        setCurrentTime(0);
+        intervals.forEach((interval) => {
+          // console.log(interval);
+          clearInterval(interval);
+        });
+      } else {
+        // console.log("start");
+        setIntervals((prevStates) => {
+          // if (prevStates.length > 0) {
+          //   return;
+          // }
+          const temp = setInterval(() => {
+            setCurrentTime((prevState) => prevState + 1);
+          }, 1000);
+          let ok = [...prevStates, temp];
+          console.log(ok, prevStates);
+          return ok;
+        });
       }
+    };
+  }, [buttonState.isStop]);
 
-      setCurrentTime(0);
-    }
-
-    setIsStop((prevState) => !prevState);
+  const stopWatchButtonHandler = (event) => {
+    dispatchButton({});
   };
 
   return (
     <Card className={styles["stop-watch"]}>
       <h1 className={styles.title}>{SecondsToMinutes(currentTime)}</h1>
       <button className={styles.button} onClick={stopWatchButtonHandler}>
-        {isStop ? "Start" : "Stop"}
+        {buttonState.isStop ? "Start" : "Stop"}
       </button>
     </Card>
   );
