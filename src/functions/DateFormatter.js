fa80c0b6aad18eb4fcb85f41df773f4c916c393d
ienrum <azang0506@gmail.com>
2023-07-06 20:08:23 +0900

    feat: 인자로 format 주기

--- src/functions/DateFormatter.js
+++ src/functions/DateFormatter.js
@@ -1,12 +1,20 @@
-const DateFormatter = (date) => {
+const DateFormatter = (date, format) => {
   const dateObj = new Date(date);
   const month = dateObj.getMonth() + 1;
   const day = dateObj.getDate();
   const minutes = dateObj.getMinutes();
+  const seconds = dateObj.getSeconds();
   const year = dateObj.getFullYear();
-  return `${month}/${day}/${year} ${dateObj.getHours()}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }`;
+  const dayOfWeek = dateObj.toLocaleString("en-us", { weekday: "short" });
+
+  return format
+    .replaceAll("dow", dayOfWeek)
+    .replaceAll("d", day)
+    .replaceAll("y", year)
+    .replaceAll("h", dateObj.getHours())
+    .replaceAll("min", minutes < 10 ? `0${minutes}` : minutes)
+    .replaceAll("m", month)
+    .replaceAll("s", seconds);
 };
 
 export default DateFormatter;
