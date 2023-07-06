1a5dfff3fd29445dd7add60df3fe20be83c02894
ienrum <azang0506@gmail.com>
2023-07-06 20:08:52 +0900

    refactor: datas 추가

--- src/datas/List.js
+++ src/datas/List.js
@@ -1,7 +1,27 @@
 import { TAGS } from "./Tags";
 export const defaultTimeLine = [
-  { id: 0, duration: 120, tag: TAGS.STUDY, startedAt: Date() },
-  { id: 1, duration: 30, tag: TAGS.EXERCISE, startedAt: Date() },
-  { id: 2, duration: 60, tag: TAGS.FRIENDS, startedAt: Date() },
-  { id: 3, duration: 10, tag: TAGS.WORK, startedAt: Date() },
+  {
+    id: 0,
+    duration: 120,
+    tag: TAGS.STUDY,
+    startedAt: new Date("2023-07-04 10:20:30"),
+  },
+  {
+    id: 1,
+    duration: 30,
+    tag: TAGS.EXERCISE,
+    startedAt: new Date("2023-07-02 10:20:30"),
+  },
+  {
+    id: 2,
+    duration: 60,
+    tag: TAGS.FRIENDS,
+    startedAt: new Date("2023-06-29 10:20:30"),
+  },
+  {
+    id: 3,
+    duration: 10,
+    tag: TAGS.WORK,
+    startedAt: new Date("2023-06-23 10:20:30"),
+  },
 ];
